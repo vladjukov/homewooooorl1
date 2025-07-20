2861a9f846f0727b356e9e52eb55a26dcdbfb533
Владислав <layan961@mail.ru>
2025-07-20 12:03:02 +0300

    homework2

---
+++ src/main/java/notify/EmailNotification.java
@@ -0,0 +1,16 @@
+package notify;
+
+public class EmailNotification extends Notification{
+    public String email;
+
+    public EmailNotification (String email, String message) {
+        super(message, Priority.HIGH);
+        this.email = email;
+    }
+    @Override
+    public void send() {
+        System.out.println("Sending e-mail to " + email);
+        super.send();
+    }
+
+}
---
+++ src/main/java/notify/Notification.java
@@ -0,0 +1,48 @@
+package notify;
+
+public class Notification {
+    private final int id = 1;
+    private String message;
+    private Priority priority;
+    private static int nextId;
+
+    public Notification () {
+        this.message = "empty";
+        this.priority = Priority.NORMAL;
+    }
+    public Notification (String message, Priority priority) {
+        this.message = message;
+        this.priority = priority;
+    }
+
+    public void send() {
+        System.out.println("Уведомление: " + message + ", приоритет: " + priority);
+    }
+    public void send(String extra) {
+        System.out.println("Уведомление: " + message + ", приоритет: " + priority + ", дополнительно: "+ extra);
+    }
+    @Override
+    public String toString() {
+        return "Уведомление {Сообщение = '" + message + "', приоритет = '" + priority + "}";
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public Priority getPriority() {
+        return priority;
+    }
+    public void setPriority (Priority priority) {
+        this.priority = priority;
+    }
+    public int getNextId () {
+        return nextId;
+    }
+
+}
