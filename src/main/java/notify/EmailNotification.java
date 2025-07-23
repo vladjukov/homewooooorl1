6a71df4078109bbec6f5def0c0212c709284ae2b
Владислав <layan961@mail.ru>
2025-07-23 11:39:26 +0300

    homework2.1

--- src/main/java/notify/EmailNotification.java
+++ src/main/java/notify/EmailNotification.java
@@ -1,12 +1,13 @@
 package notify;
 
-public class EmailNotification extends Notification{
-    public String email;
+public class EmailNotification extends Notification {
+    private final String email;
 
-    public EmailNotification (String email, String message) {
+    public EmailNotification(String email, String message) {
         super(message, Priority.HIGH);
         this.email = email;
     }
+
     @Override
     public void send() {
         System.out.println("Sending e-mail to " + email);
